021b58bc7c61cbd96959cfe30829d25711157fff
Magnus Rinnan <magnus.rinnan@nav.no>
2024-12-19 17:41:28 +0100

    Rettinger konfig

--- src/test/kotlin/no/nav/bidrag/statistikk/BidragStatistikkTest.kt
+++ src/test/kotlin/no/nav/bidrag/statistikk/BidragStatistikkTest.kt
@@ -4,12 +4,15 @@ import no.nav.bidrag.statistikk.BidragStatistikkTest.Companion.TEST_PROFILE
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.FilterType
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
+@SpringBootApplication(
+    exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class, DataSourceAutoConfiguration::class],
+)
 @ActiveProfiles(TEST_PROFILE)
 @ComponentScan(
     excludeFilters = [ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = [BidragStatistikk::class, BidragStatistikkLocal::class])],
@@ -19,6 +22,7 @@ class BidragStatistikkTest {
         const val TEST_PROFILE = "test"
     }
 }
+
 fun main(args: Array<String>) {
     val profile = if (args.isEmpty()) TEST_PROFILE else args[0]
     val app = SpringApplication(BidragStatistikkTest::class.java)
