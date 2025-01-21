c4ce494fa481e1f78d7fcc7b3619851ca1003146
Ugur Alpay Cenar <cenar93@gmail.com>
2025-01-21 15:39:44 +0100

    Fikse tester

--- src/test/kotlin/no/nav/bidrag/statistikk/hendelse/VedtakHendelseListenerTest.kt
+++ src/test/kotlin/no/nav/bidrag/statistikk/hendelse/VedtakHendelseListenerTest.kt
@@ -1,17 +1,24 @@
 package no.nav.bidrag.statistikk.hendelse
 
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
 import no.nav.bidrag.statistikk.BidragStatistikkTest
 import no.nav.bidrag.statistikk.BidragStatistikkTest.Companion.TEST_PROFILE
 import no.nav.bidrag.statistikk.TestUtil.Companion.byggVedtakDto
 import no.nav.bidrag.statistikk.TestUtil.Companion.stubHenteVedtak
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.test.context.ActiveProfiles
+import java.util.concurrent.CompletableFuture
 
 @SpringBootTest(classes = [BidragStatistikkTest::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("VedtakHendelseListener (test av forretningslogikk)")
@@ -22,6 +29,15 @@ import org.springframework.test.context.ActiveProfiles
 class VedtakHendelseListenerTest {
     @Autowired
     private lateinit var vedtakHendelseListener: VedtakHendelseListener
+
+    @MockkBean(relaxed = true)
+    private lateinit var kafkaTemplate: KafkaTemplate<String, String>
+
+    @BeforeEach
+    fun init() {
+        every { kafkaTemplate.send(any<ProducerRecord<String, String>>()) } returns CompletableFuture.completedFuture(SendResult(null, null))
+        every { kafkaTemplate.afterSingletonsInstantiated() } returns Unit
+    }
 
     @Test
     fun `skal lese vedtakshendelse uten feil`() {
