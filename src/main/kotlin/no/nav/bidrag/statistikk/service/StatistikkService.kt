4a7daa9cd1e35bb4059da9448ba9017dd42ee161
Magnus Rinnan <magnus.rinnan@nav.no>
2025-09-15 16:05:32 +0200

    Rekjører bidragsvedtak

--- src/main/kotlin/no/nav/bidrag/statistikk/service/StatistikkService.kt
+++ src/main/kotlin/no/nav/bidrag/statistikk/service/StatistikkService.kt
@@ -69,7 +69,7 @@ class StatistikkService(val hendelserService: HendelserService, val bidragVedtak
         LOGGER.info("Henter komplett vedtak for vedtaksid: ${vedtakHendelse.id}")
         SECURE_LOGGER.debug("Henter komplett vedtak for vedtaksid: {} vedtak: {}", vedtakHendelse.id, vedtakDto)
 
-        if (vedtakHendelse.id > 5050998) {
+        if (vedtakHendelse.id > 5124321) {
             behandleVedtakHendelseForskudd(vedtakHendelse, vedtakDto)
         }
         behandleVedtakHendelseBidrag(vedtakHendelse, vedtakDto)
